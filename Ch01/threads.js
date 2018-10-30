11047ec302746b347372fae27511f82c06515f8b
Vadim Storozhuk <vadim.storo@gmail.com>
2018-10-29 23:47:51 -0400

    First Commit

---
+++ Ch01/threads.js
@@ -0,0 +1,33 @@
+const crypto = require('crypto');
+
+const start = Date.now();
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', (err, result) => {
+    console.log(`1: ${Date.now() - start}`);
+
+    // const ar = [];
+    // for (let i of result) {
+    //     ar.push(i);
+    // }
+    // console.log(ar.join(' '));
+
+    // console.log(`1: typeof result: ${typeof result}`);
+    // console.log(`1: result instanceof Buffer: ${result instanceof Buffer}`);
+    // console.log(`1: result.length: ${result.length}`);
+});
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    console.log(`2: ${Date.now() - start}`);
+});
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    console.log(`3: ${Date.now() - start}`);
+});
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    console.log(`4: ${Date.now() - start}`);
+});
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    console.log(`5: ${Date.now() - start}`);
+});
